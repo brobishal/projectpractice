6e56ce444e805d9a0736ce38441d81ea6136dd1f
Bishal Shrestha <bbrobishal@gmail.com>
2023-06-01 14:55:34 +0545

    project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+function toggleHandle(){
+    // document.getElementsByClassName('toggle-btn');
+        document.getElementById('toggle').style.width="250px";
+        document.getElementById('toggle').style.display="block";
+
+        // document.getElementById('toggle').style.width="250px";
+}
+
